1588f36a286727c480838af16923cd8e0cff3bd8
AdamianArtem <adamian.artem@gmail.com>
2013-06-24 12:51:25 +0200

    pages ContactList.html and ContactList.js

---
+++ js/ContactList.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function()
+{
+	$("#addContactTop").click(function(){
+		alert("добавил");
+	});
+	
+	$("#addContactDown").click(function(){
+		alert("удалил");
+	});
+	
+	$("#remove").click(function(){
+		alert("удалил");арапорлодолж
+	});
+	
+	
+});
