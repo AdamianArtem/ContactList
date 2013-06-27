68a62cc8f5a1676672d0a2c007adf121ac83ff1c
AdamianArtem <adamian.artem@gmail.com>
2013-06-27 16:17:14 +0200

    remove id from <tr> and <a>

--- js/ContactList.js
+++ js/ContactList.js
@@ -1,29 +1,28 @@
 ﻿$(document).ready(function()
 {
 	/**
-		takes id, this id for number of record and for number of remove buttons
 		return record stored in <table> 
 		record consists of: First Name, Second Name, Phone Number, Email and remove botton
 	**/
-	function createRecord(id) {
+	function createRecord() {
 		var firstName=$("#inputFirstName").val();
 		var secondName=$("#inputSecondName").val();
 		var phoneNumber=$("#inputPhoneNumber").val();
 		var email=$("#inputEmail").val();
 		
-		$("#tableId").append("<tr id='record"+id+"'><td>"+id+"</td><td>"+firstName+"</td></td><td>"+secondName+"</td></td><td>"+phoneNumber+"</td></td><td>"+email+"</td><td><a  id='"+id+"'><i class='icon-remove'></i></a></td></tr>");
+		$("#tableId").append("<tr><td>"+id+"</td><td>"+firstName+"</td></td><td>"+secondName+"</td></td><td>"+phoneNumber+"</td></td><td>"+email+"</td><td><a><i class='icon-remove'></i></a></td></tr>");
 	}
 	
 	$('#myModal').modal({show: false});//hide modal window, when the page opens
-	var id=0;//count remove buttons
+	var id=0;//count record
 	
 	$("#AddContactBotton").click(function(){
 		id++;
-		createRecord(id);
+		createRecord();
 		
-		$("#"+id).click(function(){
-			var removeId=$(this).attr("id");// removeId stores the id remove button
-			$("#record"+removeId).remove();
+		$("td a").click(function(){
+			var tr=$(this).parent().parent();//var removeId=$(this).attr("id");// removeId stores the id remove button
+			$(tr).remove();
 		});
 	});
 });
