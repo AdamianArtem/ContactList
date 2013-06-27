77eaed9d05e9a1f796f2da1e07a9ec6df0ffc118
AdamianArtem <adamian.artem@gmail.com>
2013-06-27 17:03:44 +0200

    remove id

--- js/ContactList.js
+++ js/ContactList.js
@@ -1,14 +1,15 @@
 ﻿$(document).ready(function()
 {
 	/**
+		takes number of record
 		return record stored in <table> 
 		record consists of: First Name, Second Name, Phone Number, Email and remove botton
 	**/
-	function createRecord() {
-		var firstName=$("#inputFirstName").val();
-		var secondName=$("#inputSecondName").val();
-		var phoneNumber=$("#inputPhoneNumber").val();
-		var email=$("#inputEmail").val();
+	function createRecord(id) {
+		var firstName=$("input[placeholder='First Name']").val();
+		var secondName=$("input[placeholder='Second Name']").val();
+		var phoneNumber=$("input[placeholder='Phone Number']").val();
+		var email=$("input[placeholder='Email']").val();
 		
 		$("#tableId").append("<tr><td>"+id+"</td><td>"+firstName+"</td></td><td>"+secondName+"</td></td><td>"+phoneNumber+"</td></td><td>"+email+"</td><td><a><i class='icon-remove'></i></a></td></tr>");
 	}
@@ -18,7 +19,7 @@
 	
 	$("#AddContactBotton").click(function(){
 		id++;
-		createRecord();
+		createRecord(id);
 		
 		$("td a").click(function(){
 			var tr=$(this).parent().parent();//var removeId=$(this).attr("id");// removeId stores the id remove button
