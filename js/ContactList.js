ca05b3824e091a33edc74142be8cb699ba4b947e
AdamianArtem <adamian.artem@gmail.com>
2013-07-01 15:45:30 +0200

    create function numeration() remove unnecessary code

--- js/ContactList.js
+++ js/ContactList.js
@@ -1,38 +1,35 @@
 ﻿$(document).ready(function()
 {
 	/**
-		takes number of record
 		return record stored in <table> 
 		record consists of: First Name, Second Name, Phone Number, Email and remove botton
 	**/
-	function createRecord(id) {
+	function createRecord() {
 		var firstName=$("input[name='firstName']").val();
 		var secondName=$("input[name='secondName']").val();
 		var phoneNumber=$("input[name='phoneNumber']").val();
 		var email=$("input[name='email']").val();
 		
-		$("#contactTable").append("<tr><td class='rowIndex'>"+id+"</td><td>"+firstName+"</td></td><td>"+secondName+"</td></td><td>"+phoneNumber+"</td></td><td>"+email+"</td><td><a class='removeButton'><i class='icon-remove'></i></a></td></tr>");
+		$("#contactTable").append("<tr><td class='rowIndex'></td><td>"+firstName+"</td></td><td>"+secondName+"</td></td><td>"+phoneNumber+"</td></td><td>"+email+"</td><td><a class='removeButton'><i class='icon-remove'></i></a></td></tr>");
 	}
-	
+	/**
+		numbering row in table whith id="contactTable"
+		return number in first column
+	**/
+	function numeration() {
+		$("#contactTable .rowIndex").each(function(i) {
+				$(this).text(i+1);
+		});
+	}
 	$('#myModal').modal({show: false});//hide modal window, when the page opens
-	var id=0;//count record
-	
 	$("#AddContactBotton").click(function(){
-		
-		id++;
-		createRecord(id);
+		createRecord();
+		numeration();
 		$("#myModal input").val("");
 		
 		$(".removeButton:last").click(function(){
-			
 			$(this).parent().parent().remove();
-			
-			// numeration: correctly  numbers when removing
-			$("#contactTable .rowIndex").each(function(i) {
-				$(this).text(i+1);
-			});
-			id--;  
-			////////////////////
+			numeration();
 		});
 	});
 	
