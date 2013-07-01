ebc11cfe052934e1443299d7f3c655b30f08a60b
AdamianArtem <adamian.artem@gmail.com>
2013-07-01 15:57:09 +0200

    delete comments

--- js/ContactList.js
+++ js/ContactList.js
@@ -1,9 +1,5 @@
 ﻿$(document).ready(function()
 {
-	/**
-		return record stored in <table> 
-		record consists of: First Name, Second Name, Phone Number, Email and remove botton
-	**/
 	function createRecord() {
 		var firstName=$("input[name='firstName']").val();
 		var secondName=$("input[name='secondName']").val();
@@ -12,17 +8,16 @@
 		
 		$("#contactTable").append("<tr><td class='rowIndex'></td><td>"+firstName+"</td></td><td>"+secondName+"</td></td><td>"+phoneNumber+"</td></td><td>"+email+"</td><td><a class='removeButton'><i class='icon-remove'></i></a></td></tr>");
 	}
-	/**
-		numbering row in table whith id="contactTable"
-		return number in first column
-	**/
+	
 	function numerateRows() {
 		$("#contactTable .rowIndex").each(function(i) {
 				$(this).text(i+1);
 		});
 		
 	}
+	
 	$('#myModal').modal({show: false});//hide modal window, when the page opens
+	
 	$("#AddContactBotton").click(function(){
 		createRecord();
 		numerateRows();
